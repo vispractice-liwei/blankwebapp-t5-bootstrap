82d4df4db7dda8fbc027c1b608f4a37ae02dfe63
liwei <vispractice_liwei@126.com>
2013-08-19 10:24:59 +0800

    增加jpa示例

--- src/main/java/com/vispractice/pages/About.java
+++ src/main/java/com/vispractice/pages/About.java
@@ -1,6 +1,31 @@
 package com.vispractice.pages;
 
+import java.util.Iterator;
+
+import javax.inject.Inject;
+
+import org.apache.tapestry5.annotations.AfterRender;
+import org.apache.tapestry5.annotations.SetupRender;
+
+import com.vispractice.domain.User;
+import com.vispractice.repository.SimpleUserRepository;
+
 public class About
 {
 
+	@Inject
+	private SimpleUserRepository sur;
+	
+	@SetupRender
+	void before(){
+		sur.save(new User(System.currentTimeMillis()));
+	}
+	
+	@AfterRender
+	void after(){
+		Iterator<User> ui = sur.findAll().iterator();
+		while(ui.hasNext()){
+			System.out.println(ui.next());
+		}
+	}
 }
